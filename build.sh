daa940068ac404a2fed9d07d7735cdc35aad1c8c
BuildTools <gw@communitylabs.de>
2022-12-23 12:22:19 +0100

    fixe sed stuff

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -r .tmp
+
 # create tmp dir
 mkdir .tmp
 
@@ -23,24 +25,26 @@ for f in *.md; do
     # echo "::::::file $f\n"
     ## {{<notice note überschrift hier>}} -> ::: note\n###überschrift
     ## note -> note
-    sed -i -E "s/\{\{<\s*notice\s+note\s+(.*)>\}\}/::: note  \n\n### \1\n/g" $f
+    sed -i.bak -E "s/{{<[[:space:]]*notice[[:space:]]+note[[:space:]]+(.*)>}}/::: note  \n\n### \1\n/g" $f
     ## task -> note
-    sed -i -E "s/\{\{<\s*notice\s+task\s+(.*)>\}\}/::: note  \n\n### \1\n/g" $f
+    sed -i.bak -E "s/{{<[[:space:]]*notice[[:space:]]+task[[:space:]]+(.*)>}}/::: note  \n\n### \1\n/g" $f
     ## warning -> warning
-    sed -i -E "s/\{\{<\s*notice\s+warning\s+(.*)>\}\}/::: warning  \n\n### \1\n/g" $f
+    sed -i.bak -E "s/{{<[[:space:]]*notice[[:space:]]+warning[[:space:]]+(.*)>}}/::: warning  \n\n### \1\n/g" $f
     ## success -> tip
-    sed -i -E "s/\{\{<\s*notice\s+success\s+(.*)>\}\}/::: tip  \n\n### \1\n/g" $f
+    sed -i.bak -E "s/{{<[[:space:]]*notice[[:space:]]+success[[:space:]]+(.*)>}}/::: tip  \n\n### \1\n/g" $f
     ## {{< /notice >}} => :::
-    sed -i -E "s/\{\{<\s*\/\s*notice\s*>\}\}/:::/g" $f
+    sed -i.bak -E "s/{{<[[:space:]]*\/[[:space:]]*notice[[:space:]]*>}}/:::/g" $f
 
     
 done
 
 # convert with pandoc
 pandoc *.md -o ../output.docx
+pandoc *.md -o "../output.pdf" --from markdown --template "../eisvogel.latex" --filter pandoc-latex-environment --listings
+
 
 # mission accomplished, leave .tmp/
 cd ..
 
 # get rid of .tmp/
-rm -r .tmp
+# rm -r .tmp
